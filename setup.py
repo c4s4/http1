3b44666ddf39bded2644ef4e8e15661ee9bb57a8
John Vandenberg <jayvdb@gmail.com>
2020-11-04 07:40:42 +0700

    setup.py: Use GitHub url (#3)
    There is no benefit for PyPI package
    to link to the same PyPI package.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author = 'Michel Casabianca',
     author_email = 'casa@sweetohm.net',
     packages = ['http1'],
-    url = 'http://pypi.python.org/pypi/http1/',
+    url = 'https://github.com/c4s4/http1',
     license = 'Apache Software License',
     description = 'http1 is an API to perform HTTP requests in a single call',
     long_description=open('README.rst', encoding='UTF-8').read(),
